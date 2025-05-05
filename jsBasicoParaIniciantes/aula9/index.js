68549d9874ad2df97e40c6620fa7d6cd4b6badf1
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-05-05 14:38:37 -0300

    Tipos de dados primitivos

--- jsBasicoParaIniciantes/aula9/index.js
+++ jsBasicoParaIniciantes/aula9/index.js
@@ -1,8 +1,40 @@
-var nome = "Luiz";
-var nome = "Otávio";
+var variavel = "Luiz";
+var variavel = "Otávio";
 
 // var permite a criação do mesmo nome de variável
 
-console.log(nome);
+console.log(variavel);
 
-nome = "Luiz"; // Por default a variável vem como var
+variavel = "Luiz"; // Por default a variável vem como var
+
+const nome = "Luiz"; // string
+const nome1 = "Jaco"; // string
+const nome2 = "Jose"; // string
+
+const num1 = 10; // number
+const num2 = 10.53; // number
+
+let nomeAluno; // undefined = não é apontado para nenhum local na memória
+const sobreNomeAluno = null; // Nulo -> não aponta para nenhum local na memória
+const aprovado = true; // Valor booelano explicito --- true, false (lógico)
+
+console.log(typeof aprovado, aprovado);
+
+let a = 2;
+const b = a;
+console.log(a, b);
+
+a = 3;
+console.log(a, b);
+
+/*
+
+const a = [1, 2];
+const b = a;
+
+console.log(a, b);
+
+b.push(3);
+console.log(a,b);
+
+*/
