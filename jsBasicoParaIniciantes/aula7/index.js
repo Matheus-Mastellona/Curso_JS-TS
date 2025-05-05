2d6c2852a322971cf97b7bc6cacd41c734595cc9
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-05-05 13:55:46 -0300

    Constantes com const

---
+++ jsBasicoParaIniciantes/aula7/index.js
@@ -0,0 +1,13 @@
+const nome = "João"; // o valor da constante não pode ser alterado
+// constante deve ser atribuida o valor ao iniciar
+console.log(nome);
+
+// da erro => const nome;
+// da errp => consto nome = "x"; - nome = 'y'
+
+console.log(typeof(nome)); // verificando a tipagem da variável
+
+const numero = '4';
+const segundoNumero = 50;
+
+console.log(numero+segundoNumero);// concatenação, pois são variáveis de tipagens diferentes
