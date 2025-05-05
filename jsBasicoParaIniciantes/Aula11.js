0d617f3a4a309bf0b2b5480124f072b166940d9a
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-05-05 15:44:52 -0300

    Exercícios - Solução

--- jsBasicoParaIniciantes/Aula11.js
+++ jsBasicoParaIniciantes/Aula11.js
@@ -7,9 +7,12 @@ window.prompt('Digite seu nome?');
 let num1 = prompt("Digite o 1° número: ");
 let num2 = prompt("Digite o 2° número: ");
 
+console.log(num1);
+console.log(num2);
+
 num1 = Number(num1);
 num2 = Number(num2);
 
-const conta = num1 + num2;
+//const conta = num1 + num2;
 
-alert("Resultado: " + conta);
+alert(`Resultado: ${num1 + num2}`);
