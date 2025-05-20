a3e25321c863a67ca6ed48ae3970c8a91bb19f4a
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-05-20 15:23:17 -0300

    Operadores de comparação

---
+++ logicaDeProgramacao/OperadoresDeComparacao.js
@@ -0,0 +1,15 @@
+/* 
+Operadores de comparação
+> maior que
+>= maior que o igual a 
+< menor que
+<= menor que ou igual a 
+== igualdade ( valor )
+=== igualdade estrita ( valor e tipo )
+!= diferente ( valor )
+!== diferente estrito ( valor e tipo )
+*/
+
+const num1 = 1;
+const num2 = 1;
+const comp = num1 === num2;
