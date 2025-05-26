6d7471bcf10b346f9a436dbec37ead2dd64cbf90
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-05-26 14:51:02 -0300

    For - Clássico - Estrutura de repetição

---
+++ estudos/logicaDeProgramacao/ClassicFor.js
@@ -0,0 +1,18 @@
+/* 
+ for (let i = 0; i <= 5; i++) {
+  console.log(`Linha ${i}`);
+} 
+ */
+/* 
+for (let i = 0; i <= 10; i++) {
+  let num = i % 2 === 0 ? "par" : "ímpar";
+  console.log(`Index: ${i} number: ${num}`);
+}
+ */
+
+
+const frutas = ["Maçã", "Pêra", "Uva"];
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Índice ${i}`, frutas[i]);
+}
