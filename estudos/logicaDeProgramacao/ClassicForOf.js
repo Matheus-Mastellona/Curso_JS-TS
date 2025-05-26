a7cdc4fff99434780a8c8d80c5fec8edf4c67750
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-05-26 15:51:23 -0300

    For of

---
+++ estudos/logicaDeProgramacao/ClassicForOf.js
@@ -0,0 +1,33 @@
+/* const nome = "Luiz Otávio"; */
+
+/* for (let index = 0; index < nome.length; index++) {
+  console.log(nome[index]);
+} */
+
+/* for (const key in nome) {
+    console.log(nome[key]);
+} */
+
+/* for (const key of nome) {
+    console.log(key);
+} */
+
+/* const nomes = ["Matheus", "Noronha", "Souza"];
+
+nomes.forEach(function (el, index, array) {
+  console.log(el, index, array);
+}); */
+
+/* const pessoa = {
+  nome: "Matheus",
+  sobrenome: "Noronha",
+  idade: 22,
+};
+
+for (const key in pessoa) {
+   console.log(key, pessoa[key]);
+}
+
+For clássico - Geralmente com iteráveis (arrays ou strings)
+For in - Retorna o índice ou chave (string, array ou objetos)
+For of - Retorna o valor em si (iteráveis, arrays ou strings) */
