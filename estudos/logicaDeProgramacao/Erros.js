0c7d6788da74e5bbf1f656b8d8044e97be032ac3
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-05-28 08:49:46 -0300

    Tratando e lançando erros (try, catch, throw)

---
+++ estudos/logicaDeProgramacao/Erros.js
@@ -0,0 +1,11 @@
+function soma(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw new Error("x e y precisam ser números");
+  }
+  return x + y;
+}
+try {
+    console.log(soma('oi',2));
+} catch (error) {
+    console.log('Erro');
+}
